d4318e2b12288558638b518cdb2c3e5cd01f225a
Unai Perez <uperez@certiorem.com>
2020-03-23 08:22:28 +0100

    Primer commit

---
+++ DbSql-service/src/main/java/com/springboot/app/dbsql/DbSqlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.dbsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSqlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbSqlServiceApplication.class, args);
+	}
+
+}
---
+++ DbSql-service/src/test/java/com/springboot/app/dbsql/DbSqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.dbsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSqlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
